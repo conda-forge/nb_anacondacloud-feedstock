70f7937cdec647f573446d0c83bc3d33d3ea7320
Nicholas Bollweg <nbollweg@continuum.io>
2016-06-13 22:37:45 -0400

    use block-style output redirect

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -1,2 +1,4 @@
-"${PREFIX}/bin/jupyter-nbextension" enable nb_anacondacloud --py --sys-prefix > /dev/null 2>&1
-"${PREFIX}/bin/jupyter-serverextension" enable nb_anacondacloud --py --sys-prefix > /dev/null 2>&1
+{
+  "${PREFIX}/bin/jupyter-nbextension" enable nb_anacondacloud --py --sys-prefix
+  "${PREFIX}/bin/jupyter-serverextension" enable nb_anacondacloud --py --sys-prefix
+} >>"$PREFIX/.messages.txt" 2>&1
